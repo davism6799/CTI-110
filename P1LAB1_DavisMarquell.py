8c95d5bd09e620687733dbe37273ebb11071c950
davism6799 <up5routes@icloud.com>
2024-06-20 13:37:00 -0400

    Add files via upload

---
+++ P1LAB1_DavisMarquell.py
@@ -0,0 +1,11 @@
+
+# Marquell Davis
+# 6/18/2024
+# P1LAB1
+#program displays a welcome using the user's
+
+
+first_name= input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
